37090e388424de2e8377fac3dced5c42e16f2948
Oleh Sliusar <oleh@olehsliusar.com>
2017-02-22 14:55:37 +0200

    Add my_inject method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -69,4 +69,59 @@ module Enumerable
     end
     self
   end
+
+  def my_inject(arg = nil, arg2 = nil)
+    if !arg.nil? && !(Symbol === arg)
+      memo = arg
+    end
+    memo = 0
+    memo = 1 if arg == :*
+    if Symbol === arg
+      self.my_each_with_index do |element, index|
+        memo = self[0] if index == 0
+        memo = memo.send arg, self[index + 1] unless self[index + 1].nil?
+      end
+      memo
+    elsif Symbol === arg2
+      memo = arg
+      self.my_each do |element|
+        memo = memo.send arg2, element
+      end
+      memo
+    elsif !arg.nil? && arg2.nil? && block_given?
+      memo = arg
+      self.my_each do |element|
+        memo = yield(memo, element)
+      end
+      memo
+    else
+      memo = 0
+      self.my_each_with_index do |element, index|
+        memo = self[0] if index == 0
+        memo = yield(memo, element)
+      end
+      memo
+    end
+  end
 end
+
+p "[1, 2, 3, 4].my_map { |i| i*i } => #{[1, 2, 3, 4].my_map { |i| i*i }}, should be [1, 4, 9, 16]"   #=> [1, 4, 9, 16]
+
+p "[5, 6, 7, 8, 9, 10].my_inject { |sum, n| sum + n }    => #{[5, 6, 7, 8, 9, 10].my_inject { |sum, n| sum + n }}, should be 45"   #=> 45
+
+p "[5, 6, 7, 8, 9, 10].my_inject(:+)   => #{[5, 6, 7, 8, 9, 10].my_inject(:+)}, should be 45"    # => 45
+
+p "[10, 2, 4].my_inject(:-)    => #{[10, 2, 4].my_inject(:-)}, should be 4"    # => 4
+
+p "[8, 2, 2].my_inject(:/)    => #{[8, 2, 2].my_inject(:/)}, should be 2"   # => 2
+
+p "[1, 2, 3, 4].my_inject(:*)   => #{[1, 2, 3, 4].my_inject(:*)}, should be 24"    # => 24
+
+p "[5, 6, 7, 8, 9, 10].my_inject(1, :*)    => #{[5, 6, 7, 8, 9, 10].my_inject(1, :*)}, should be 151200"    # => 151200
+
+p "[5, 6, 7, 8, 9, 10].my_inject(1) { |product, n| product * n }   => #{[5, 6, 7, 8, 9, 10].my_inject(1) { |product, n| product * n }}, should be 151200" #=> 151200
+
+longest = %w{ cat sheep bear }.my_inject do |memo, word|
+   memo.length > word.length ? memo : word
+end
+p "The longest word is '#{longest}', should be 'sheep'"
